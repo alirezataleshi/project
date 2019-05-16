1a37ce9d6d626ef8e6a225fb6d1329fea93a62c8
alirezataleshi <rezasalehani@outlook.com>
2019-05-17 03:25:51 +0430

    sec 1

---
+++ 1.py
@@ -0,0 +1,37 @@
+person = {"id":93203923902, "name": "mamad", "lastname": "jafari", }
+print(person["name"])
+print(person["id"])
+
+lst= [1, 2, 3, 4, 5]
+print(lst[-2])
+print(lst[-3])
+
+test = {1, "mamad", 4, 5}
+print(test)
+
+print(len(test))
+print(len(person))
+
+
+myint = 7
+
+print(myint)
+
+mystring = "hello"
+print(mystring)
+
+
+
+one = 1
+two = 2
+three = one + two
+print(three)
+
+
+
+helllllllllllllo = "hello" * 11
+print(helllllllllllllo)
+
+
+
+print([1, 3, 2] * 3)
