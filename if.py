e177b048657987c54617afa83e0af0457df84f0d
alirezataleshi <rezasalehani@outlook.com>
2019-05-17 13:03:57 +0430

    split

--- if.py
+++ if.py
@@ -1,16 +1,28 @@
-weight = 100
+weight = input("enter weight:")
 
-height = 1.7
+height =float(input("enter height:"))
 
-result = weight / height**2
-
+result=(weight/(height**2))
 print(result)
 
 if result<30:
     print("under")
 
-if 32 < result < 70:
+elif result>30:
     print("ok")
 
 if result > 70:
     print("over")
+
+
+if.py:16
+
+
+
+
+
+
+
+exam="tommy tommy hardy hardy is awesome"
+
+exam=exam.split()
