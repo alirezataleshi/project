0d567815041aedda95ba024f99ed28c1aa938602
alirezataleshi <rezasalehani@outlook.com>
2019-05-17 11:24:11 +0430

    bmi

---
+++ if.py
@@ -0,0 +1,16 @@
+weight = 100
+
+height = 1.7
+
+result = weight / height**2
+
+print(result)
+
+if result<30:
+    print("under")
+
+if 32 < result < 70:
+    print("ok")
+
+if result > 70:
+    print("over")
