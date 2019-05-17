9529e05508a65197f80c2c0286a7ff5bb618a656
alirezataleshi <rezasalehani@outlook.com>
2019-05-17 11:09:37 +0430

    sec-2

---
+++ and-or.py
@@ -0,0 +1,20 @@
+start = 10
+
+end = 20
+
+startc = 30
+
+endd = 40
+
+num= 22
+
+test = (start<num<end) or (startc<num<endd)
+
+print(test)
+
+
+num2 = 11
+
+test2 = (start<num2<end) or (startc<num2<endd)
+
+print(tes
