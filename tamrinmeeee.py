a36a55fc318ec36174e25049b968462543ae17a4
alirezataleshi <rezasalehani@outlook.com>
2019-06-01 20:28:58 +0430

    tamrin meeeee

---
+++ tamrinmeeee.py
@@ -0,0 +1,44 @@
+def my_function():
+
+     print(4**2)
+
+my_function()
+
+
+
+
+def my_func(keshvar="iran"):
+    print("iam from" + keshvar)
+
+
+my_func("blarus")
+my_func("ghana")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def my_ffunc(z):
+ return 10**z
+
+my_ffunc(2)
