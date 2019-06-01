a58f56f7d6eb28d85a7f542f2aa1cb918610e8c7
alirezataleshi <rezasalehani@outlook.com>
2019-06-01 17:48:45 +0430

    tmarin me

---
+++ tamrin.me.py
@@ -0,0 +1,41 @@
+x="hardy"
+print("tom"+x)
+
+
+y=24j
+print(type(y))
+
+
+a="tom, hardy"
+
+print(a[2:8])
+
+rr="salam chetori"
+print(len(rr))
+
+print(rr.upper())
+
+print(rr.replace("s", "d"))
+
+
+
+thislist= ["apple", "banana"]
+print(thislist[1])
+
+thislist[0]="sib"
+print(thislist)
+
+
+
+if "sib"  in thislist:
+    print("y")
+
+
+    print(len(thislist))
+
+
+
+    thislist.append("orange")
+    
+   thislist.insert(1, "peach")
+   print(thislist)
