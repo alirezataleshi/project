0b9be07f3ad257294a357a65e47e511c58f4e2b9
alirezataleshi <rezasalehani@outlook.com>
2019-07-04 17:15:28 +0430

    poem

--- poem.py
+++ poem.py
@@ -5,3 +5,16 @@ print(words)
 words2=set(open("poem.txt", errors="ignore").read().split())
 print(words2)
 print(len(words2))
+
+file=open("poem.txt", "r", errors="ignore")
+
+wordcount={}
+
+for word in file.read().split():
+    if word not in wordcount:
+        wordcount[word] = 1
+    else:
+        wordcount[word] += 1
+
+for k,v in wordcount.items():
+    print(k, v)
